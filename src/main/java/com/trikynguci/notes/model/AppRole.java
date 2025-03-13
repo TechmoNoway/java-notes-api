e98e3f3d14e77cffdabfecd6f354cf6be908a88c
Ky Nguyen <cuty0406@outlook.com>
2025-03-13 23:19:29 +0700

    Add more models and services for user

---
+++ src/main/java/com/trikynguci/notes/model/AppRole.java
@@ -0,0 +1,6 @@
+package com.trikynguci.notes.model;
+
+public enum AppRole {
+    ROLE_USER,
+    ROLE_ADMIN
+}
---
+++ src/main/java/com/trikynguci/notes/service/Impl/UserDetailsImpl.java
@@ -0,0 +1,31 @@
+package com.trikynguci.notes.service.Impl;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@NoArgsConstructor
+@Data
+public class UserDetailsImpl implements UserDetails {
+    private static final long serialVersionUID = 1L;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+}
